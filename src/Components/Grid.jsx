e9cdd0f091faaa6ff1648b5fb0d12fdb1d7b1919
NicKyeaa <tin.jurcevic@gmail.com>
2022-01-29 14:10:26 +0100

    Changed heading for table

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -22,29 +22,29 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
 
-function createData(name, calories, fat, carbs, protein) {
+function createData(category, inventoryNumber, model, serialNumber, location) {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    category,
+    inventoryNumber,
+    model,
+    serialNumber,
+    location,
   };
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Skener', 305, 3.7, 67, 4.3),
+  createData('', 452, 25.0, 51, 4.9),
+  createData('Skener', 262, 16.0, 24, 6.0),
+  createData('Skener', 159, 6.0, 24, 4.0),
   createData('Gingerbread', 356, 16.0, 49, 3.9),
   createData('Honeycomb', 408, 3.2, 87, 6.5),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
+  createData('Skener', 375, 0.0, 94, 0.0),
   createData('KitKat', 518, 26.0, 65, 7.0),
   createData('Lollipop', 392, 0.2, 98, 0.0),
   createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
+  createData('Skener', 360, 19.0, 9, 37.0),
   createData('Oreo', 437, 18.0, 63, 4.0),
 ];
 
@@ -80,34 +80,34 @@ function stableSort(array, comparator) {
 
 const headCells = [
   {
-    id: 'name',
+    id: 'category',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)',
+    label: 'Category',
   },
   {
-    id: 'calories',
+    id: 'inventoryNumber',
     numeric: true,
     disablePadding: false,
-    label: 'Calories',
+    label: 'Inventurni broj',
   },
   {
-    id: 'fat',
-    numeric: true,
+    id: 'model',
+    numeric: false,
     disablePadding: false,
-    label: 'Fat (g)',
+    label: 'Model',
   },
   {
-    id: 'carbs',
+    id: 'serialNumber',
     numeric: true,
     disablePadding: false,
-    label: 'Carbs (g)',
+    label: 'Serijski broj',
   },
   {
-    id: 'protein',
-    numeric: true,
+    id: 'location',
+    numeric: false,
     disablePadding: false,
-    label: 'Protein (g)',
+    label: 'Lokacija',
   },
 ];
 
