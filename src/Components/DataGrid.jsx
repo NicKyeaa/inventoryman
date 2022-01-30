5153bfa8cf1ffb9f5ad943d922376fd46f24eecf
Tin <tin.jurcevic@gmail.com>
2022-01-30 22:33:45 +0100

    Added DataGrid component and Rows And Columns

--- src/Components/DataGrid.jsx
+++ src/Components/DataGrid.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: 'category',
+        headerName: 'Kategorija',
+        width: 150,
+    },
+    {
+        field: 'inventoryNumber',
+        headerName: 'Inventurni broj',
+        width: 100,
+    }
+];
+
+const rows = [
+    { id:1 , category: 'Skener', inventoryNumber: 200}  
+];
+
 export default function InvGrid() {
-  return <div></div>;
+    return (
+        <div style={{ height: 400, width: '100%'}}>
+            <DataGrid>
+                rows={rows}
+                columns={columns}
+            </DataGrid>
+        </div>
+  );
 }
