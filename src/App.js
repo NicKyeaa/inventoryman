8dae7c8f4cf23725fa43a46a7ca14a1fd305e796
Tin <tin.jurcevic@gmail.com>
2022-02-14 15:23:55 +0100

    Modified modal for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';  
 // import Grid from './Components/Grid';
-import InvGrid from './Components/InvGrid';
+// import InvGrid from './Components/InvGrid';
+import AddModal from './Components/AddModal';
 
 function App() {
   return (
     <div className="App">
       {/* <Grid /> */}
-      <InvGrid />
+      {/* <InvGrid /> */}
+      <AddModal />
     </div>
   );
 }
--- src/Components/AddModal.jsx
+++ src/Components/AddModal.jsx
@@ -8,22 +8,10 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   return (
     <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={true}>
         <DialogTitle>Subscribe</DialogTitle>
         <DialogContent>
           <DialogContentText>
@@ -41,8 +29,8 @@ export default function FormDialog() {
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button>Cancel</Button>
+          <Button>Subscribe</Button>
         </DialogActions>
       </Dialog>
     </div>
