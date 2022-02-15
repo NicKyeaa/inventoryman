e140698fa0a2da9ac14acfb9f642812bc2727c1f
Tin <tin.jurcevic@gmail.com>
2022-02-15 12:14:49 +0100

    Added select button for Category

--- src/Components/AddModal.jsx
+++ src/Components/AddModal.jsx
@@ -6,6 +6,11 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default function AddModal(props) {
     return (
@@ -16,15 +21,17 @@ export default function AddModal(props) {
                     <DialogContentText>
                         Here you can Add New Asset
                     </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="New Asset"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
+                    <Box sx={{ minWidth: 120 }}>
+                        <InputLabel id="categoryInput">Kategorija</InputLabel>
+                        <Select
+                            labelId="categoryInput"
+                            id="categoryInput-select"
+                            // value={age}
+                            label="Kategorija"
+                            // onChange={handleChange}
+                        >
+                        </Select>
+                    </Box>
                 </DialogContent>
             <DialogActions>
             <Button onClick={props.closeModal}>Cancel</Button>
